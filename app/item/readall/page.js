7188cdcc1d918227e708bb854dc137bfe76e5fef
Eiichi Horita <ehorita@blue.ocn.ne.jp>
2024-12-14 18:07:42 +0900

    Modifying app/item/readall.js etc.

--- app/item/readall/page.js
+++ app/item/readall/page.js
@@ -8,92 +8,77 @@
 // By Horita.
 // On (2024 Nov 30).
 //######################################################################
-'use client'
+'use client';//Client Component.
 //**********************************************************************
 //This is a server component.
 const trcLev = 1;
 //const trcLev = 2;
 const mode = 0;
+const path = 'app/item/readall/page';//Added
 //======================================================================
 const url0 = process.env.NEXT_PUBLIC_URL;//New
 const url1 = 'api/item/readall';//New
 const url2 = `/${url1}`;//New for test.
-//const url2 = '/api/item/readall';//New for test.
-//const url2 = './api/item/readall';//New for test.
-//const url2 = `${url0}/${url1}`;//New
-//const url = 'http://localhost:3000/api/item/readall';
-//======================================================================
-const path = 'app/item/readall/page';//Added
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+import { useState, useEffect } from 'react';//Added
 import Link   from 'next/link';//Added
 import Image  from 'next/image';//Added
 import Header from '../../components/header';//Added
 import Footer from '../../components/footer';//Added
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-async function getAllItems(){
-  const response = await fetch(url2);//Mdf
-  //const response = await fetch(url);
-  {cache: "no-store"};//Added
-  const jsonData = await response.json();
-  if( trcLev >= 2 ){
-    console.log(`-- ${path}.getAllItems()#1:jsonData=`); 
-    console.dir(jsonData);//Tracing
-  }
-  return jsonData;//Added
-}//Added
-//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-//**********************************************************************
 //v This WORKS for for [npm run start]
 function Page(){//<1
-  //======================================================================
-  async function handleSubmit(evt){//<2
-    evt.preventDefault() 
-    const response = await fetch(url2);//Mdf
-    //const response = await fetch(url);
-    let items = await getAllItems();
-    let message = items.message;
-    let items1 = items.allItems;
-    let lgt1 = items1.length;
-    if( mode === 1 ){
-      alert(`-- ${path}.handleSubmit()#1:lgt1=${lgt1}.`);
-    }
-    let elmData = document.getElementById('data');
-    let htmlText = '';
-    //----------------------------------------------------------------------
-    for(let k=0; k < lgt1; ++k){//______________________________BGN <LOOP.1>
-      let item1 = items1[k];
-      let id1 = item1._id;
-      let link1 = `/item/readsingle2/${id1}`;//New
-      //let link1 = `/item/readsingle/${id1}`;
-      let title1 = item1.title;
-      let description = item1.description;
-      let description1 = description.substr(0,60)+' ....';//Mdf
-      let email1 = item1.email;
-      let price1 = item1.price;
-      let image1 = item1.image;
-      if( image1.substr(0,1) !== '/' ){
-        image1 = '/' + image1;
-      }
-      htmlText +=
-        `<div key=${k+1}>\n`+
-          `<p>#${k+1}，Id：${id1}，<a href=${link1}>Link for details</a></p>\n`+
-          `<p>Title：${title1}，Price：${price1}円，Email：${email1}</p>\n`+
-          `<p>Description：${description1}</p>\n`+
-          `<p>Image："${image1}"</p>\n`+
-          `<p><Image src=${image1} width=150 height=100 alt='item-image' priority/></p>\n`+
-          `<hr/>\n`+
-        `</div>\n`;
-    }//_________________________________________________________END <LOOP.1>
-    elmData.innerHTML = htmlText;//New
-  }//2>
+  useEffect( () =>//<2
+    {//<3
+      //======================================================================
+      async function getAllItems(){//<4
+        const response = await fetch(url2);//Mdf
+        //{cache: "no-store"};//Added
+        const jsonData = await response.json();
+        let message = jsonData.message;
+        let items1 = jsonData.allItems;
+        let lgt1 = items1.length;
+        if( mode === 1 ){
+          alert(`-- ${path}.getAllItems()#1:lgt1=${lgt1}.`);
+        }
+        let elmData = document.getElementById('data');
+        let htmlText = '';
+        //----------------------------------------------------------------------
+        for(let k=0; k < lgt1; ++k){//______________________________BGN <LOOP.1>
+          let item1 = items1[k];
+          let id1 = item1._id;
+          let link1 = `/item/readsingle2/${id1}`;//New
+          //let link1 = `/item/readsingle/${id1}`;
+          let title1 = item1.title;
+          let description = item1.description;
+          let description1 = description.substr(0,60)+' ....';//Mdf
+          let email1 = item1.email;
+          let price1 = item1.price;
+          let image1 = item1.image;
+          if( image1.substr(0,1) !== '/' ){
+            image1 = '/' + image1;
+          }
+          htmlText +=
+            `<div key=${k+1}>\n`+
+              `<p>#${k+1}，Id：${id1}，<a href=${link1}>Link for details</a></p>\n`+
+              `<p>Title：${title1}，Price：${price1}円，Email：${email1}</p>\n`+
+              `<p>Description：${description1}</p>\n`+
+              `<p>Image："${image1}"</p>\n`+
+              `<p><Image src=${image1} width=150 height=100 alt='item-image' priority/></p>\n`+
+              `<hr/>\n`+
+            `</div>\n`;
+        }//_________________________________________________________END <LOOP.1>
+        elmData.innerHTML = htmlText;//New
+      }//4>
+      getAllItems();
+    },//3>
+    []
+  );//2>
   //======================================================================
   return (
     <>
       <Header/>
       <h3 style={{color:'red'}}>アイテム一覧：</h3>
-      <form onSubmit={handleSubmit}>
-        <button >Show all items.</button>
-      </form>
       <div id='data'></div>
       <Footer/>
     </>
--- app/item/readsingle2/[id]/page.js
+++ app/item/readsingle2/[id]/page.js
@@ -8,8 +8,9 @@
 // By Horita.
 // On (2024 Dec 1).
 //######################################################################
-'use client'
+'use client';//Client Component.
 // server component
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 const trcLev = 2;
 const path = 'app/item/readsingle2/_/page';
 //======================================================================
@@ -18,98 +19,75 @@ const url1 = 'api/item/readsingle2';//New
 const url2 = `/${url1}`;
 //const url2 = `${url0}/${url1}`;
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+import { useState, useEffect } from 'react';//Added
 import Link   from 'next/link';//Added
 import Image  from 'next/image';//Added
 import Header from '../../../components/header';//Added
 import Footer from '../../../components/footer';//Added
 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-async function getSingleItem(context){
-  const params = await context.params;
-  const id = params.id;
-  const headers1 =
-    { 
-      'Accept': 'application/json', 
-      'Content-Type': 'application/json',
-    };
-  const body1 =
-    {
-      id: id
-    };
-  const ob1 =
-    {
-      method: 'POST',
-      headers: headers1,
-      body: JSON.stringify(body1)
-    };
-  const resp = await fetch(url2, ob1);//New
-  const jsonData = await resp.json();
-  return jsonData;
-}
-//**********************************************************************
 function Page(context){//<1
   //======================================================================
-  async function handleSubmit(evt){//<2
-    evt.preventDefault() 
-
-    const params = await context.params;
-    const id = params.id;
-    const headers1 =
-      { 
-        'Accept': 'application/json', 
-        'Content-Type': 'application/json',
-      };
-    const body1 =
-      {
-        id: id
-      };
-    const ob1 =
-      {
-        method: 'POST',
-        headers: headers1,
-        body: JSON.stringify(body1)
-      };
-    const resp = await fetch(url2, ob1);//New
-    const jsonData = await resp.json();
-    const jsonData1 = jsonData.message;
-    const jsonData2 =jsonData.singleItem;
-    if( jsonData2 !== undefined ){
-      const id = jsonData2._id;
-      const title = jsonData2.title;
-      let image = jsonData2.image;
-      if( image.substr(0,1) !== '/' ){//<3
-        image = '/' + image;
-      }//3>
-      const price = jsonData2.price;
-      const description = jsonData2.description;
-      const email = jsonData2.email;
-      const link = '/';
-      let htmlText = '';
-      htmlText += `<p>${jsonData1}</p>\n`;
-      htmlText += `<p>Id：${id}</p>\n`;
-      htmlText += `<p>Title：${title}</p>\n`;
-      htmlText += `<p>Price：${price}</p>\n`;
-      htmlText += `<p>Email：${email}</p>\n`;
-      htmlText += `<p>Description：${description}</p>\n`;
-      htmlText += `<p>Image："${image}"</p>\n`;
-      htmlText += `<img src=${image} width=750 height=500 alt='item-image' priority/>\n`;
-      htmlText += `<p>Link：<a href="${link}">Top-page</Link></a>\n`;
-      let elmData = document.getElementById('data');
-      elmData.innerHTML = htmlText;//New
-    }
-    else{
-      let htmlText = '';
-      htmlText += jsonData1;
-      elmData.innerHTML = htmlText;//New      
-    }
-  }//2>
+  useEffect( () => 
+    {
+      async function getSingleItem(){//<2
+        const params = await context.params;
+        const id = params.id;
+        const headers1 =
+          { 
+            'Accept': 'application/json', 
+            'Content-Type': 'application/json',
+          };
+        const body1 = { id: id };
+        const ob1 =
+          {
+            method: 'POST',
+            headers: headers1,
+            body: JSON.stringify(body1)
+          };
+        const resp = await fetch(url2, ob1);//New
+        const jsonData = await resp.json();
+        const jsonData1 = jsonData.message;
+        const jsonData2 =jsonData.singleItem;
+        if( jsonData2 !== undefined ){
+          const id = jsonData2._id;
+          const title = jsonData2.title;
+          let image = jsonData2.image;
+          if( image.substr(0,1) !== '/' ){//<3
+            image = '/' + image;
+          }//3>
+          const price = jsonData2.price;
+          const description = jsonData2.description;
+          const email = jsonData2.email;
+          const link = '/';
+          let htmlText = '';
+          htmlText += `<p>${jsonData1}</p>\n`;
+          htmlText += `<p>Id：${id}</p>\n`;
+          htmlText += `<p>Title：${title}</p>\n`;
+          htmlText += `<p>Price：${price}</p>\n`;
+          htmlText += `<p>Email：${email}</p>\n`;
+          htmlText += `<p>Description：${description}</p>\n`;
+          htmlText += `<p>Image："${image}"</p>\n`;
+          htmlText += `<img src=${image} width=750 height=500 alt='item-image' priority/>\n`;
+          htmlText += `<p>Link：<a href="${link}">Top-page</Link></a>\n`;
+          let elmData = document.getElementById('data');
+          elmData.innerHTML = htmlText;//New
+        }
+        else{
+          let htmlText = '';
+          htmlText += `<p>${jsonData1}</p>\n`;//Mdf
+          elmData.innerHTML = htmlText;//New      
+        }
+      }//2>
+      getSingleItem();
+    }, 
+    []           
+    //[context]
+  );
   //======================================================================
   return (
     <>
       <Header/>
       <h3 style={{color:'red'}}>個別アイテム：</h3>
-      <form onSubmit={handleSubmit}>
-        <button >Show this item.</button>
-      </form>
       <div id='data'></div>
       <Footer/>
     </>
